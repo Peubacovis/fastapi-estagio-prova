861c518a9f0b4410c8d7f11951793e90b4990df7
Peubacovis <peubacovis@gmail.com>
2025-02-17 14:14:15 -0300

    :wrench: feat Criação de arquivos de configuração de banco de dados

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import schemas
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+@app.post("/empresas/", response_model=schemas.Empresa)
+def create_empresa(empresa: schemas.EmpresaCreate, db: Session = Depends(get_db)):
+    db_empresa = models.Empresa(**empresa.dict())
+    db.add(db_empresa)
+    db.commit()
+    db.refresh(db_empresa)
+    return db_empresa
+
+@app.get("/empresas/{empresa_id}", response_model=schemas.Empresa)
+def read_empresa(empresa_id: int, db: Session = Depends(get_db)):
+    db_empresa = db.query(models.Empresa).filter(models.Empresa.id == empresa_id).first()
+    if db_empresa is None:
+        raise HTTPException(status_code=404, detail="Empresa não encontrada")
+    return db_empresa
+
+@app.post("/obrigacoes/", response_model=schemas.ObrigacaoAcessoria)
+def create_obrigacao(obrigacao: schemas.ObrigacaoAcessoriaCreate, db: Session = Depends(get_db)):
+    db_obrigacao = models.ObrigacaoAcessoria(**obrigacao.dict())
+    db.add(db_obrigacao)
+    db.commit()
+    db.refresh(db_obrigacao)
+    return db_obrigacao
+
+@app.get("/obrigacoes/{obrigacao_id}", response_model=schemas.ObrigacaoAcessoria)
+def read_obrigacao(obrigacao_id: int, db: Session = Depends(get_db)):
+    db_obrigacao = db.query(models.ObrigacaoAcessoria).filter(models.ObrigacaoAcessoria.id == obrigacao_id).first()
+    if db_obrigacao is None:
+        raise HTTPException(status_code=404, detail="Obrigação não encontrada")
+    return db_obrigacao
